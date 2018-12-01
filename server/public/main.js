db6a59d33fc02bc58376474d60aa51ab9ee918bd
Mattemyo <matias97marti@gmail.com>
2018-12-01 14:47:59 +0100

    :art: more changes

--- server/public/main.js
+++ server/public/main.js
@@ -2,12 +2,12 @@
 const SPINNER_TIMEOUT = 4000;
 
 // =============== first page here ======== //
-const contentWrapper = document.querySelector('.content');
+const welcomeFormWrapper = document.querySelector('.welcome-form');
 const spinnerWrapper = document.querySelector('.spinner');
-const submitButton = document.querySelector('button');
+const submitButton = document.querySelector('.login');
 
 submitButton.addEventListener('click', () => {
-  [contentWrapper, spinnerWrapper].forEach((el) => el.classList.toggle('hidden'));
+  [welcomeFormWrapper, spinnerWrapper].forEach((el) => el.classList.toggle('hidden'));
   setTimeout(() => {
     window.location.pathname = 'results';
   }, SPINNER_TIMEOUT);
