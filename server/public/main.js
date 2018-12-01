f933dfd28335d84dd6c8a035ccd034858657d1f5
Mattemyo <matias97marti@gmail.com>
2018-12-01 15:09:56 +0100

    :art: add spinner animation and image

--- server/public/main.js
+++ server/public/main.js
@@ -12,3 +12,16 @@ submitButton.addEventListener('click', () => {
     window.location.pathname = 'results';
   }, SPINNER_TIMEOUT);
 });
+
+// animate spinner dots
+const dots = document.getElementById('moving-dots');
+
+let num = 0;
+setInterval(() => {
+  dots.textContent = '.'.repeat(num);
+  if (num === 3) {
+    num = 0;
+  } else {
+    num += 1;
+  }
+}, 200);
